2c655206e39c8773956e42bd29e324a76d068062
AlexFP <florianprescott@googlemail.com>
2017-12-20 18:16:55 -0500

    added sounds and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,6 +35,12 @@ $(document).ready(function() {
 
 	var that;
 
+	//audio prepared
+	const attackAudio = document.createElement("audio");
+   $(attackAudio).attr("src", "assets/audio/attack_sound.mp3");
+   const characterAudio = document.createElement("audio");
+   $(characterAudio).attr("src", "assets/audio/character_sound.mp3");
+
 	console.log(playerHP);
 	console.log(playerAtkPwr);
 	console.log(defenderHP); 
@@ -45,6 +51,8 @@ $(document).ready(function() {
 
 	//player selects character, player variables defined, the rest are moved to available defenders
 	function chooseCharacter() {
+
+		characterAudio.play();
 
 		//convert chosen character thumbnail into object and change id of player character assciated thumbnail
 		var playerSelectId = that.id;
@@ -98,7 +106,9 @@ $(document).ready(function() {
 		console.log("defender HP: " + defenderHP);
 
 		$("#player_health").html("Hit Points: " + playerHP);
-		$("#player_attack").html("Attack Power: " + playerAtkPwr); 
+		$("#player_attack").html("Attack Power: " + playerAtkPwr);
+
+
 
 	}
 
@@ -155,8 +165,10 @@ $(document).ready(function() {
 	//creates attack button, which engages attacking parameters when button is used
 	function attackDefender() {
 
-		$("#attackButton").html("<button class='btn btn-default' id='attack_function' type='button'>Attack!</button>");
+		$("#attackButton").html("<button class='btn btn-danger btn-lg' id='attack_function' type='button'>Attack!</button>");
 		$("#attack_function").click(function() {
+
+			attackAudio.play();
 
 			defenderHP = defenderHP - playerAtkPwr;
 			console.log("Defender has " + defenderHP + " hit points");
@@ -183,7 +195,8 @@ $(document).ready(function() {
 					$("#pageTitle3").empty();
 					$("#attackButton").empty();
 					$("#pageTitle1").html(playerSelect.name + " is victorious!");
-					$("#player").animate({height: '300px', width: '300px'}, "slow");				
+					$("#player").animate({height: '300px', width: '300px'}, "slow");	
+
 					resetGame();
 				} 				
 
@@ -214,6 +227,7 @@ $(document).ready(function() {
 
 					console.log(playerSelect.name + " has lost!");
 					$("#pageTitle2").html("Your hero has fallen!");
+
 					resetGame();
 				}
 			}
@@ -223,7 +237,7 @@ $(document).ready(function() {
 	//resets game after victory or defeat
 	function resetGame() {
 
-		$("#attackButton").html("<button class='btn btn-default' id='reset' type='button'>Play again?</button>");
+		$("#attackButton").html("<button type='button' class='btn btn-danger' id='reset'>Play again?</button>");
 		$("#reset").click(function() {
 
 			playerHP = "no player HP";
@@ -258,11 +272,12 @@ $(document).ready(function() {
 			$("#enemy_health").empty();
 			$("#enemy_counter").empty();
 
+			//resets character selection
 			$("#pageTitle1").html("Select Your Character");
-			$("#starting_characters").append('<a href="#" class="thumbnail pull-left character_img" id="luke"><img src="assets/images/luke-skywalker.jpg" alt="luke"></a>');				
-			$("#starting_characters").append('<a href="#" class="thumbnail pull-left character_img" id="vader"><img src="assets/images/darth-vader.jpg" alt="vader"></a>');				
-			$("#starting_characters").append('<a href="#" class="thumbnail pull-left character_img" id="obi-wan"><img src="assets/images/obi-wan.jpg" alt="obi-wan"></a>');				
-			$("#starting_characters").append('<a href="#" class="thumbnail pull-left character_img" id="palpatine"><img src="assets/images/emperor-palpatine.jpeg" alt="palpatine"></a>');
+			$("#starting_characters").append('<a href="#" class="thumbnail pull-left character_img" id="luke"><img src="assets/images/luke-skywalker.jpg" alt="luke"><div class="bottom-left"><p>Luke Skywalker</p><p>Attack:   10</p><p>Counter:  8</p><p>Health:   50</p></div></a>');				
+			$("#starting_characters").append('<a href="#" class="thumbnail pull-left character_img" id="vader"><img src="assets/images/darth-vader.jpg" alt="vader"><div class="bottom-left"><p>Darth Vader</p><p>Attack:   8</p><p>Counter:  16</p><p>Health:   60</p></div></a>');				
+			$("#starting_characters").append('<a href="#" class="thumbnail pull-left character_img" id="obi-wan"><img src="assets/images/obi-wan.jpg" alt="obi-wan"><div class="bottom-left"><p>Obi-Wan Kenobi</p><p>Attack:   6</p><p>Counter:  12</p><p>Health:   70</p></div></a>');				
+			$("#starting_characters").append('<a href="#" class="thumbnail pull-left character_img" id="palpatine"><img src="assets/images/emperor-palpatine.jpeg" alt="palpatine"><div class="bottom-left"><p>Emperor Palpatine</p><p>Attack:   4</p><p>Counter:  20</p><p>Health:   80</p></div></a>');
 
 			playGame();
 
